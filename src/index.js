b1d4f79561131f55567eaaa37c676adf60484e3b
omkarvashistha <omkarvashistha90@gmail.com>
2024-10-19 22:00:11 +0530

    added changes on 19 oct 2024

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import { animate , inView, stagger } from 'motion';
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+
+inView('.timeline',target => {
+    animate('.timeline-item',
+        {
+            opacity : [0,1],
+            height : ['0' , '230px']
+        },
+        {
+            delay : stagger(0.6),
+            easing : "ease-in",
+            duration : 1
+        }
+    );
+})
+
+
+
